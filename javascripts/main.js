a2570391fb97122adff329742348fdecb8f56351
Yohannes Libanos <4yohannes@gmail.com>
2015-01-07 00:30:21 +0000

    added main.js file

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+function displayArticle(title, body){
+
+var template="<article><h3>%title%</h3><span>%body%</span></article>".replace('%title%', title).replace('%body%', body);
+
+
+document.getElementById("main-content").innerHTML += template;
+
+
+
+}
+
+displayArticle("Hello World!","Hmmm... This is interesting");
